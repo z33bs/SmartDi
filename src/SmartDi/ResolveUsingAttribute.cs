cf2dd5c16db62bdb874424c4dac5b9a6ce48f6dc
Zeebs <zeebsmail@gmail.com>
2020-05-17 12:30:03 +0200

    Import Pilot Code

---
+++ src/SmartDi/ResolveUsingAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace SmartDi
+{
+    /// <summary>
+    /// Attribute to explicitly mark which constructor should be used by the <see cref="Ioc"/>
+    /// to instantiate the class
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Constructor,AllowMultiple = false)]
+    public class ResolveUsingAttribute : Attribute
+    {
+        /// <summary>
+        /// Attribute to explicitly mark which constructor should be used by the <see cref="Ioc"/>
+        /// to instantiate the class
+        /// </summary>
+        public ResolveUsingAttribute()
+        {
+        }
+    }
+}
